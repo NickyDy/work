0ce077e2c03413ab2288a59f6e2f65a5249a23b2
NickyDy <nickydyakov@gmail.com>
2025-05-29 09:16:57 +0300

    new commit

--- croatia.R
+++ croatia.R
@@ -1,92 +1,99 @@
 library(tidyverse)
 library(nanoparquet)
+options(scipen = 100)
 
 rps <- read_parquet("work/rps.parquet")
 rgfi <- read_parquet("work/rgfi.parquet")
 
-glimpse(rgfi)
+comp <- rgfi %>% 
+  select(pla_13, pla_11, liabilities_a7) %>% 
+  mutate(comp = pla_13 == pla_11)
+
+comp %>% count(comp)
+
+glimpse(rgfi_2023)
 rps %>% count(oblik_vlasništva, sort = T)
 
-rgfi %>% 
-  filter(mb %in% c(3536386, 1968823)) %>% 
-  ggplot(aes(year, assets_c, group = mb)) +
-  geom_point() +
-  geom_line()
+rgfi_year_na <- rgfi %>% 
+  reframe(across(everything(), ~ round(sum(is.na(.) / n() * 100), 3)), .by = year) %>% 
+  as_tibble() %>%
+  pivot_longer(-year, names_to = "index", values_to = "perc_na") %>% 
+  arrange(year, perc_na) %>%
+  select(index, year, perc_na) %>% 
+  pivot_wider(names_from = year, values_from = perc_na)
 
-rgfi_na <- rgfi %>%
+df_na <- df %>%
   map_dfr(~ sum(is.na(.))) %>% 
   pivot_longer(everything()) %>%
   mutate(perc_na = round(value / nrow(rgfi) * 100, 2)) %>% 
   arrange(perc_na)
-#---------------
-rgfi %>% count(assets_b3, sort = T)
+
+rgfi %>% 
+  #mutate(mb = paste0("00", mb)) %>% 
+  filter(mb == "1968823") %>%
+  pivot_longer(-c(1:4)) %>% 
+  drop_na(value) %>% 
+  ggplot(aes(dat_predaje, value)) +
+  geom_line() +
+  geom_point() +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y") +
+  facet_wrap(vars(name), scales = "free_y", ncol = 5)
+#------------------------------------------
+df %>% count(oib, sort = T)
 
 glimpse(rgfi_2023)
 
 rps <- rps %>% 
-  filter(rb == 0000)
+  filter(rb == 0000) %>% 
+  distinct()
 
-df_2023 <- rgfi_2023 %>% 
+df_croatia <- rgfi %>% 
   left_join(., rps, by = "mb")
 
-df_2023 <- df_2023 %>% 
+df_croatia <- df_croatia %>% 
   select(oib, mb, dat_predaje, year, rb:oblik_vlasništva_naziv, everything())
-
-df_joined %>% count(Pravno_ustrojbeni_oblik_naziv, sort = T)
-
-df_joined_na <- df_joined %>%
-  #filter(year == "2023") %>% 
-  map_dfr(~ sum(is.na(.))) %>% 
-  pivot_longer(everything()) %>% 
-  mutate(perc_na = round(value / 558610 * 100, 2)) %>% 
-  arrange(perc_na)
-
-perc_NA <- rgfi_clean %>%
-  #filter(year == "2023") %>% 
-  map_dfr(~ sum(is.na(.))) %>% 
-  pivot_longer(everything()) %>% 
-  mutate(perc_na = round(value / 595774 * 100, 2)) %>% 
-  arrange(perc_na)
-
-df_2023_na <- df_2023 %>%
-  #filter(year == "2023") %>% 
-  map_dfr(~ sum(is.na(.))) %>% 
-  pivot_longer(everything()) %>% 
-  mutate(perc_na = round(value / 88316 * 100, 2)) %>% 
-  arrange(perc_na)
 
 write_parquet(rps, "work/rps.parquet")
 write_parquet(rgfi, "work/rgfi.parquet")
-write_csv(df_2023, "work/rgfi_2023.csv")
+write_csv(rgfi, "work/rgfi.csv")
 #---------------------------------------
-rgfi_2023 <- read_csv2("work/RGFI_javna_objava_2023_new.csv") %>%
+rgfi_2023 <- read_csv2("work/RGFI_javna_objava_2023.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2023. - 31.12.2023."),
          ozn_kons == 0) %>% 
   select(oib, mb, dat_predaje, contains(c("tg1", "tg2"))) %>%
   mutate(dat_predaje = dmy(dat_predaje), year = 2023) %>% 
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2022 <- read_csv2("work/RGFI_javna_objava_2022_new.csv") %>%
+rgfi_2022 <- read_csv2("work/RGFI_javna_objava_2022.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2022. - 31.12.2022."),
          ozn_kons == 0) %>% 
   select(oib, mb, dat_predaje, contains(c("tg1", "tg2"))) %>%
   mutate(dat_predaje = dmy(dat_predaje), year = 2022) %>% 
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2021 <- read_csv2("work/RGFI_javna_objava_2021_new.csv") %>%
+rgfi_2021 <- read_csv2("work/RGFI_javna_objava_2021.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2021. - 31.12.2021."),
          ozn_kons == 0) %>% 
   select(oib, mb, dat_predaje, contains(c("tg1", "tg2"))) %>%
   mutate(dat_predaje = dmy(dat_predaje), year = 2021) %>% 
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2020 <- read_csv2("work/RGFI_javna_objava_2020_new.csv") %>%
+rgfi_2020 <- read_csv2("work/RGFI_javna_objava_2020.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2020. - 31.12.2020."),
          ozn_kons == 0) %>% 
@@ -105,10 +112,12 @@ rgfi_2020 <- read_csv2("work/RGFI_javna_objava_2020_new.csv") %>%
          aop_tg2_184 = aop_tg2_182, aop_tg2_185 = aop_tg2_183,
          aop_tg2_205 = aop_tg2_203, aop_tg2_223 = aop_tg2_213,
          aop_tg2_224 = aop_tg2_214) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2019 <- read_csv2("work/RGFI_javna_objava_2019_new.csv") %>%
+rgfi_2019 <- read_csv2("work/RGFI_javna_objava_2019.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2019. - 31.12.2019."),
          ozn_kons == 0) %>% 
@@ -127,10 +136,12 @@ rgfi_2019 <- read_csv2("work/RGFI_javna_objava_2019_new.csv") %>%
          aop_tg2_184 = aop_tg2_182, aop_tg2_185 = aop_tg2_183,
          aop_tg2_205 = aop_tg2_203, aop_tg2_223 = aop_tg2_213,
          aop_tg2_224 = aop_tg2_214) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2018 <- read_csv2("work/RGFI_javna_objava_2018_new.csv") %>%
+rgfi_2018 <- read_csv2("work/RGFI_javna_objava_2018.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2018. - 31.12.2018."),
          ozn_kons == 0) %>% 
@@ -149,10 +160,12 @@ rgfi_2018 <- read_csv2("work/RGFI_javna_objava_2018_new.csv") %>%
          aop_tg2_184 = aop_tg2_182, aop_tg2_185 = aop_tg2_183,
          aop_tg2_205 = aop_tg2_203, aop_tg2_223 = aop_tg2_213,
          aop_tg2_224 = aop_tg2_214) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2017 <- read_csv2("work/RGFI_javna_objava_2017_new.csv") %>%
+rgfi_2017 <- read_csv2("work/RGFI_javna_objava_2017.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2017. - 31.12.2017."),
          ozn_kons == 0) %>% 
@@ -171,10 +184,12 @@ rgfi_2017 <- read_csv2("work/RGFI_javna_objava_2017_new.csv") %>%
          aop_tg2_184 = aop_tg2_182, aop_tg2_185 = aop_tg2_183,
          aop_tg2_205 = aop_tg2_203, aop_tg2_223 = aop_tg2_213,
          aop_tg2_224 = aop_tg2_214) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2016 <- read_csv2("work/RGFI_javna_objava_2016_new.csv") %>%
+rgfi_2016 <- read_csv2("work/RGFI_javna_objava_2016.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2016. - 31.12.2016."),
          ozn_kons == 0) %>% 
@@ -193,10 +208,12 @@ rgfi_2016 <- read_csv2("work/RGFI_javna_objava_2016_new.csv") %>%
          aop_tg2_184 = aop_tg2_182, aop_tg2_185 = aop_tg2_183,
          aop_tg2_205 = aop_tg2_203, aop_tg2_223 = aop_tg2_213,
          aop_tg2_224 = aop_tg2_214) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2015 <- read_csv2("work/RGFI_javna_objava_2015_new.csv") %>%
+rgfi_2015 <- read_csv2("work/RGFI_javna_objava_2015.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2015. - 31.12.2015."),
          ozn_kons == 0) %>% 
@@ -221,10 +238,12 @@ rgfi_2015 <- read_csv2("work/RGFI_javna_objava_2015_new.csv") %>%
          aop_tg2_185 = aop_tg2_152, aop_tg2_202 = aop_tg2_157,
          aop_tg2_205 = aop_tg2_158, aop_tg2_223 = aop_tg2_167,
          aop_tg2_224 = aop_tg2_168) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2014 <- read_csv2("work/RGFI_javna_objava_2014_new.csv") %>%
+rgfi_2014 <- read_csv2("work/RGFI_javna_objava_2014.csv", 
+                       quote = "\'", na = "\"\"") %>% 
+  janitor::clean_names() %>% 
+  mutate(across(everything(), ~str_remove_all(., '\"'))) %>% 
   filter(vr_izvj == 10, 
          str_detect(naz_izvjestaja2, "01.01.2014. - 31.12.2014."),
          ozn_kons == 0) %>% 
@@ -249,10 +268,10 @@ rgfi_2014 <- read_csv2("work/RGFI_javna_objava_2014_new.csv") %>%
          aop_tg2_185 = aop_tg2_152, aop_tg2_202 = aop_tg2_157,
          aop_tg2_205 = aop_tg2_158, aop_tg2_223 = aop_tg2_167,
          aop_tg2_224 = aop_tg2_168) %>%
-  mutate(across(-dat_predaje, as.numeric)) %>% 
-  distinct()
+  mutate(across(-dat_predaje, as.numeric))
 
-rgfi_2014 <- rgfi_2014 %>% 
+rgfi <- bind_rows(rgfi_2023, rgfi_2022, rgfi_2021, rgfi_2020, rgfi_2019, rgfi_2018,
+                  rgfi_2017, rgfi_2016, rgfi_2015, rgfi_2014) %>% 
   select(oib, mb, dat_predaje, year, assets_a = aop_tg1_2, assets_b1 = aop_tg1_3,
          assets_b2 = aop_tg1_10, assets_b3 = aop_tg1_20, assets_c = aop_tg1_37,
          assets_c1 = aop_tg1_38, assets_c2 = aop_tg1_46, assets_c3 = aop_tg1_53, assets_c4 = aop_tg1_63,
@@ -262,11 +281,10 @@ rgfi_2014 <- rgfi_2014 %>%
          liabilities_d = aop_tg1_109, liabilities_e = aop_tg1_124, liabilities_f = aop_tg1_125,
          pla_1 = aop_tg2_127, pla_2 = aop_tg2_133, pla_3 = aop_tg2_156, pla_4 = aop_tg2_167,
          pla_9 = aop_tg2_179, pla_10 = aop_tg2_180,
-         pla_11 = aop_tg2_181, pla_12 = aop_tg2_184, pla_13 = aop_tg2_185)
+         pla_11 = aop_tg2_181, pla_12 = aop_tg2_184, pla_13 = aop_tg2_185) %>% 
+  distinct()
 
 glimpse(rgfi)
 
 write_csv(rgfi, "work/rgfi.csv")
-
-rgfi <- bind_rows(rgfi_2023, rgfi_2022, rgfi_2021, rgfi_2020, rgfi_2019, rgfi_2018,
-                  rgfi_2017, rgfi_2016, rgfi_2015, rgfi_2014)
+write_parquet(rgfi, "work/rgfi.parquet")

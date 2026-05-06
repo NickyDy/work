781d2e282a9ed11a4e88e70c45d64611867eaf63
NickyDy <nickydyakov@gmail.com>
2026-05-06 06:41:03 +0300

    6 may, 2026

--- s_p.R
+++ s_p.R
@@ -1,9 +1,16 @@
 library(tidyverse)
+library(nanoparquet)
 library(fs)
 library(readxl)
+
 options(scipen = 100)
 
-portugal <- read_csv("work/portugal.csv")
+portugal <- read_parquet("work/portugal.parquet")
+#write_csv(portugal, "work/portugal.csv")
+
+portugal <- left_join(balance_sheet, income_statement %>% select(-c(2, 4:7)), by = c("year", "sp_entity_id"))
+
+glimpse(portugal)
 
 read_excel_col <- function(file){
   read_excel(file, col_names = c("entity_name", "entity_id", "geograhphy", "company_status",
@@ -12,7 +19,9 @@ read_excel_col <- function(file){
 }
 
 read_excel_names <- function(file){
-  read_excel(file, skip = 4, na = "NA") %>% 
+  read_excel(file, 
+             skip = 4, 
+             na = "NA") %>% 
     slice(-c(1:2)) %>% 
     janitor::clean_names()
 }
@@ -22,17 +31,14 @@ df <- map(files, read_excel_names) %>%
   set_names(basename) %>%
   list_rbind(names_to = "year") %>%
   mutate(year = str_extract(year, "\\d+")) %>% 
-  mutate(across(1:6, as.character)) %>% 
-  mutate(across(-c(1:6), as.numeric)) %>% 
+  mutate(across(1:7, as.character)) %>% 
+  mutate(across(-c(1:7), as.numeric)) %>% 
   mutate(across(where(is.numeric), \(x) round(x, 2)))
 
 glimpse(df)
-portugal %>%
-  drop_na(total_rev, net_inc, total_assets, 
-          total_debt, total_equity) %>% 
-  count(entity_id) %>% 
-  filter(n >= 9) %>% 
-  view
+
+portugal %>% count(sp_entity_id) %>% view
+
 
 plot_id("4434302")
 
@@ -55,14 +61,14 @@ plot_id <- function(number) {
     theme(text = element_text(size = 16))
 }
 
-df %>%
+portugal %>%
   map_dfr(~ sum(is.na(.))) %>%
   pivot_longer(everything()) %>%
-  mutate(perc_na = round(value / nrow(df) * 100, 2)) %>%
+  mutate(perc_na = round(value / nrow(portugal) * 100, 2)) %>%
   arrange(perc_na) %>% 
   print(n = Inf)
 
-df %>%
+income_statement %>%
   reframe(across(everything(), ~ round(sum(is.na(.) / n() * 100), 3)), .by = year) %>%
   as_tibble() %>%
   pivot_longer(-year, names_to = "index", values_to = "perc_na") %>%
@@ -71,4 +77,4 @@ df %>%
   pivot_wider(names_from = year, values_from = perc_na) %>% 
   print(n = Inf)
 
-#write_csv(df, "work/portugal.csv")
+#write_parquet(portugal, "work/portugal.parquet")

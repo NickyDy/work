ee76f8a46c8ce8ec13aa41b890f5d33bf345e98f
NickyDy <nickydyakov@gmail.com>
2026-05-19 07:40:09 +0300

    may 19, 2026

--- s_p.R
+++ s_p.R
@@ -6,7 +6,8 @@ library(readxl)
 options(scipen = 100)
 
 portugal <- read_parquet("work/portugal.parquet")
-#write_csv(portugal, "work/portugal.csv")
+portugal_na <- read_parquet("work/portugal_na.parquet")
+portugal_csv <- read_csv("work/portugal.csv")
 
 portugal <- left_join(balance_sheet, income_statement %>% select(-c(2, 4:7)), by = c("year", "sp_entity_id"))
 
@@ -35,28 +36,31 @@ df <- map(files, read_excel_names) %>%
   mutate(across(-c(1:7), as.numeric)) %>% 
   mutate(across(where(is.numeric), \(x) round(x, 2)))
 
-glimpse(df)
+glimpse(portugal)
 
 portugal %>% count(sp_entity_id) %>% view
 
 
-plot_id("4434302")
+plot_id("8026915")
 
 plot_id <- function(number) {
   title <- portugal %>%
-    filter(entity_id == number)
+    filter(sp_entity_id == number)
   
   portugal %>%
-    filter(entity_id == number) %>%
-    pivot_longer(total_rev:total_equity) %>%
-    drop_na() %>%
+    filter(sp_entity_id == number) %>%
+    pivot_longer(c(pcd_total_current_assets, pcd_total_assets, pcd_total_common_equity, pcd_total_equity,
+                   pcd_total_liab_equity, pcd_total_ca_reported, pcd_total_assets_reported,
+                   pcd_total_stock_holders_equity_reported, pcd_total_liabilities_reported,
+                   pcd_total_current_liab, pcd_total_liab, pcd_retained_earnings, pcd_total_cl_reported)) %>%
+    drop_na(value) %>%
     mutate(col = value > 0, name = fct_inorder(name)) %>%
-    ggplot(aes(year, value, fill = col, group = name)) +
+    ggplot(aes(as.numeric(year), value)) +
     geom_point(show.legend = F) +
     geom_line(linetype = 2, linewidth = 0.2) +
     scale_x_continuous(n.breaks = 10) +
     labs(x = "Година", y = "Стойност",
-         title = paste0("ENTITY ID: ", unique(title$entity_id))) +
+         title = paste0("ENTITY NAME: ", unique(title$sp_entity_name))) +
     facet_wrap(vars(name), scales = "free_y", ncol = 3) +
     theme(text = element_text(size = 16))
 }
@@ -66,9 +70,10 @@ portugal %>%
   pivot_longer(everything()) %>%
   mutate(perc_na = round(value / nrow(portugal) * 100, 2)) %>%
   arrange(perc_na) %>% 
+  #filter(perc_na == 100) %>% 
   print(n = Inf)
 
-income_statement %>%
+portugal_na %>%
   reframe(across(everything(), ~ round(sum(is.na(.) / n() * 100), 3)), .by = year) %>%
   as_tibble() %>%
   pivot_longer(-year, names_to = "index", values_to = "perc_na") %>%
@@ -77,4 +82,5 @@ income_statement %>%
   pivot_wider(names_from = year, values_from = perc_na) %>% 
   print(n = Inf)
 
-#write_parquet(portugal, "work/portugal.parquet")
+#write_parquet(portugal_na, "work/portugal_na.parquet")
+#write_csv(portugal, "work/portugal.csv")
